01e7136794d19620caaf782be5e44c838f75fbea
James Lucktaylor <jlucktay+github@gmail.com>
2026-01-11 11:11:06 +0000

    feat: 🎸 Cover more error cases in remove script

--- remove_previous_exit_node.sh
+++ remove_previous_exit_node.sh
@@ -1,23 +1,34 @@
 #!/usr/bin/env bash
-set -eux -o pipefail
+set -euo pipefail
 
-# Tailscale API docs are here: https://github.com/tailscale/tailscale/blob/main/api.md
+# Tailscale API docs are here: https://tailscale.com/api
 
-if [[ -z $TAILSCALE_API_KEY ]]; then
-  echo >&2 "No API key found for Tailscale; exiting."
+if [[ -z ${TAILSCALE_API_KEY-} ]]; then
+  echo >&2 "No API key found for Tailscale in the 'TAILSCALE_API_KEY' environment variable; exiting."
   exit 1
 fi
 
-if ! tailnet_devices=$(curl --request GET --silent --user "$TAILSCALE_API_KEY:" \
-  "https://api.tailscale.com/api/v2/tailnet/-/devices"); then
-  echo >&2 "Could not get Tailnet devices; exiting."
+if ! tailnet_devices=$(curl --location --request GET --silent --user "$TAILSCALE_API_KEY:" "https://api.tailscale.com/api/v2/tailnet/-/devices"); then
+  echo >&2 "Could not get devices on Tailnet from Tailscale API; exiting."
   exit 1
 fi
 
-if ! exit_node_id=$(jq --exit-status --raw-output \
-  '.devices[] | select( .hostname == "exit-node-vm" ) | .nodeId' <<< "$tailnet_devices"); then
-  echo "No previous exit node device found on Tailnet."
+if [[ $tailnet_devices == '{"message":"API token invalid"}' ]]; then
+  echo >&2 "The API key in the 'TAILSCALE_API_KEY' environment variable is invalid; exiting."
+  exit 1
+fi
+
+if ! exit_node_id=$(jq --raw-output '.devices[] | select( .hostname == "exit-node-vm" ) | .nodeId' <<< "$tailnet_devices"); then
+  echo >&2 "Could not parse exit node ID from list of Tailnet devices; exiting."
+  exit 1
+fi
+
+if [[ -z $exit_node_id ]]; then
+  echo "No previous exit node device found on Tailnet; exiting."
   exit 0
-else
-  curl --request DELETE --user "$TAILSCALE_API_KEY:" "https://api.tailscale.com/api/v2/device/$exit_node_id"
+fi
+
+if ! curl --request DELETE --user "$TAILSCALE_API_KEY:" "https://api.tailscale.com/api/v2/device/$exit_node_id"; then
+  echo >&2 "Could not delete exit node ID '$exit_node_id' from Tailnet; exiting."
+  exit 1
 fi

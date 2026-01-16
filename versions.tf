1d9338843e2bff6513abae18d463790b27c2f8cf
James Lucktaylor <jlucktay+github@gmail.com>
2026-01-16 15:54:05 +0000

    style: 💄 Consolidate, and link some relevant notes

--- versions.tf
+++ versions.tf
@@ -1,19 +1,17 @@
+# https://docs.cloud.google.com/docs/terraform/best-practices/reusable-modules#providers-backends
+# Pin to a major version, and allow minor/patch upgrades.
+
 terraform {
-  # Pin to a major version, and allow minor/patch upgrades.
   required_version = "~> 1.0"
 
   required_providers {
     google = {
-      source = "hashicorp/google"
-
-      # Pin to a major version, and allow minor/patch upgrades.
+      source  = "hashicorp/google"
       version = "~> 7.0"
     }
 
     tailscale = {
-      source = "tailscale/tailscale"
-
-      # Pin to a major version, and allow minor/patch upgrades.
+      source  = "tailscale/tailscale"
       version = "~> 0.0"
     }
   }
